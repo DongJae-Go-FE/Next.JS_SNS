d804cf581bf52af5ea073dbc09d696d23cc825c4
가동재 <masterforce999@naver.com>
2024-11-01 17:03:45 +0900

    auth 처음부터 다시해야함

--- src/app/(beforeLogin)/_components/LoginModal/LoginModal.tsx
+++ src/app/(beforeLogin)/_components/LoginModal/LoginModal.tsx
@@ -1,31 +1,68 @@
 "use client";
+
 import Style from "./style.module.css";
+import BackButton from "@/app/components/BackButton/BackButton";
+import { ChangeEventHandler, FormEventHandler, useState } from "react";
 import { useRouter } from "next/navigation";
+import { signIn } from "next-auth/react";
 
 export default function LoginModal() {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [message, setMessage] = useState("");
   const router = useRouter();
+
+  const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
+    e.preventDefault();
+    setMessage("");
+    try {
+      const result = await signIn("credentials", {
+        username: id,
+        password: password,
+        redirect: false,
+      });
+      console.log("결과::", result);
+      router.replace("/home");
+    } catch (err) {
+      console.error("에러", err);
+      setMessage("아이디와 비밀번호가 일치하지 않습니다.");
+    }
+  };
   const onClickClose = () => {
     router.back();
   };
+
+  const onChangeId: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setId(e.target.value);
+  };
+
+  const onChangePassword: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPassword(e.target.value);
+  };
+
   return (
     <div role="dialog" className={Style.dim}>
       <div className={Style.box}>
         로그인 모달
         <br />
-        <label htmlFor="id">아이디</label>
+        <form onSubmit={onSubmit}>
+          <label htmlFor="id">아이디</label>
+          <br />
+          <input value={id} id="id" onChange={onChangeId} type="text" />
+          <br />
+          <label htmlFor="password">비밀번호</label>
+          <br />
+          <input
+            value={password}
+            onChange={onChangePassword}
+            type="password"
+            id="password"
+            placeholder="비밀번호를 입력하세여"
+          />
+          <button type="submit">로그인하기</button>
+        </form>
         <br />
-        <input type="text" id="id" placeholder="아이디를 입력하세여" />
-        <br />
-        <label htmlFor="pw">비밀번호</label>
-        <br />
-        <input
-          type="password"
-          name="pw"
-          id="pw"
-          placeholder="비밀번호를 입력하세여"
-        />
-        <br />
-        <button onClick={onClickClose}>뒤로가기</button>
+        <BackButton />
       </div>
     </div>
   );
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -10,9 +10,9 @@ function generateDate() {
   });
 }
 const User = [
-  { id: "elonmusk", nickname: "Elon Musk", image: "/yRsRRjGO.jpg" },
-  { id: "zerohch0", nickname: "제로초", image: "/5Udwvqim.jpg" },
-  { id: "leoturtle", nickname: "레오", image: faker.image.avatar() },
+  { id: "ndj", nickname: "나동재", image: faker.image.avatar() },
+  { id: "ddj", nickname: "다동재", image: faker.image.avatar() },
+  { id: "ldj", nickname: "라동재", image: faker.image.avatar() },
 ];
 const Posts = [];
 const delay = (ms: number) =>
@@ -23,11 +23,14 @@ const delay = (ms: number) =>
 export const handlers = [
   http.post("/api/login", () => {
     console.log("로그인");
-    return HttpResponse.json(User[1], {
-      headers: {
-        "Set-Cookie": "connect.sid=msw-cookie;HttpOnly;Path=/",
-      },
-    });
+    return HttpResponse.json(
+      { id: "ddj", nickname: "다동재", image: faker.image.avatar() },
+      {
+        headers: {
+          "Set-Cookie": "connect.sid=msw-cookie;HttpOnly;Path=/",
+        },
+      }
+    );
   }),
   http.post("/api/logout", () => {
     console.log("로그아웃");
